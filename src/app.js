657b79a6dee26060fa09bed3100ca4af290f6ba4
kriss2013 <christophe.claude@googlemail.com>
2016-11-19 13:23:40 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 // Gate Jedi Peddle APP (Version 1.3) @TVMiller 
 // Pebble Blink APP @Jack-Dangerfield
 
+var Vibe = require('ui/vibe');
+
 var Accel = require('ui/accel');
 Accel.init();
 var UI = require('ui');
@@ -20,6 +22,8 @@ var txtOnLabel = new UI.Text({
     color: 'white'
 });
 
+var Sperre=0;
+
 // Display Main Window
 main_window.backgroundColor('black');
 main_window.add(txtOnLabel);
@@ -27,10 +31,11 @@ main_window.show();
 
 // URL To Particle Cloud (Token in Particle Build)
 function Toggle(function_name,function_value){
-  var URL_ON = 'http://192.168.178.86/gpio/1';
-  var URL_OFF = 'http://192.168.178.86/gpio/0';
+  var URL_ON = 'http://192.168.178.87/gpio/0';
+  var URL_OFF = 'http://192.168.178.87/gpio/1';
 
-
+if (Sperre===0){
+  Sperre=1;
   ajax(	
     {
       url: URL_ON,
@@ -39,8 +44,11 @@ function Toggle(function_name,function_value){
       data: { "args": function_value}
     }
   );
-
+  console.log(URL_ON);
+          // Send a long vibration to the user wrist
+Vibe.vibrate('long');
   setTimeout(function(){  
+    console.log(URL_OFF);
    ajax(	
     {
       url: URL_OFF,
@@ -50,19 +58,23 @@ function Toggle(function_name,function_value){
     }
     );
   },750);
-  
+  setTimeout(function(){Sperre=0;},3000);
+}
 }
 
 // Accelerometer Poll and Function (Default 100Hz 25)
 Accel.on('data', function(e) {
+  console.log("x accel:");
   console.log(e.accel.x);
   // If X Axis Force (1)
-  if (e.accel.x > 900) { 
-    console.log(e.accel.y);
+  if (e.accel.y > 900  || e.accel.y < -900) { 
+  //  console.log("y accel:");
+  //  console.log(e.accel.z);
   // If Y Axis Force (2)
-      if (e.accel.y > 900) {
-  Toggle('gate','open');
- }}
+ //     if (e.accel.z < -900) {
+    Toggle('gate','open');
+
+ }//}
 });
 
 // Button Function and Photon Parameters
